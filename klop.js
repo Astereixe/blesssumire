2040f2bfa2cd944880c7b7a55bc91d2c62a44faf
Denny Wahyu Prasetyo <43938494+janexmgd@users.noreply.github.com>
2022-09-30 13:06:39 +0700

    Create klop.js

---
+++ klop.js
@@ -0,0 +1,24 @@
+let data = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+};
+let mydata = {
+  name: "Denny Wahyu Prasetyo",
+  email: "dennywp666@gmail.com",
+  hobby: "mendengarkan musik",
+};
+
+const newData = { ...data, ...mydata };
+const { street, city } = data.address;
+console.log(newData);
+console.log(`${street} ${city}`);
