039b6de0f25cda094a2fd1574f0e0bc63c6ab657
janexmgd <janexmgd@gmail.com>
2023-10-01 14:04:23 +0700

    adding simple node web server

---
+++ simplewebserv.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 8000;
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  // console.log(res._header)
+  const header = res._header;
+  res.end(JSON.stringify(header));
+};
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Running at http://${host}:${port}`);
+});
